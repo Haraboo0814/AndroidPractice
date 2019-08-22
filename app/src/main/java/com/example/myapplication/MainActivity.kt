51e006198408ef502d9c6d91671772ebfb84c9e7
kyoheiharada <harada-kyohei-fj@ynu.jp>
2019-08-22 22:12:53 +0900

    インテントを作成する

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +16,11 @@ class MainActivity : AppCompatActivity() {
     }
     /** ユーザーが Send button を押したときに呼ばれる*/
     fun sendMessage(view: View) {
-        // Do something in response to button
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
