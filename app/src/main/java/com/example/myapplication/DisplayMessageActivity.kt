939e632a1f20354cfe2e0633f306dcaec36038ad
kyoheiharada <harada-kyohei-fj@ynu.jp>
2019-08-22 22:23:14 +0900

    メッセージを表示する

--- app/src/main/java/com/example/myapplication/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/myapplication/DisplayMessageActivity.kt
@@ -2,11 +2,20 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+
+        // このアクティビティを開始し String を展開する Intent を取得
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // レイアウトのTextViewをキャプチャし、文字列をテキストとして設定します
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
     }
 }
